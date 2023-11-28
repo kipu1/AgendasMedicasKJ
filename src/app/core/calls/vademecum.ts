55d9bbb069cd329bd90f2accb69fdf6cfedb0af9
Kevin Ortiz <111302391+kipu1@users.noreply.github.com>
2023-11-28 10:13:20 -0500

    Revert "Revert "cambios""
    This reverts commit 2c999fed453707efccb04731132f8002a89c758c.

---
+++ src/app/core/calls/vademecum.ts
@@ -0,0 +1,21 @@
+export class Vademecum {
+
+    id!: number;
+    compartido: string='';
+      
+    
+    laboratorio: string='';
+    medicamento: string='';
+    composicion: string='';
+    indicaciones: string='';
+    contraindicaciones : string='';   
+    posologia: string='';
+    equivalencias: string='';
+    anotaciones: string='';
+    stock!: number;  
+    
+   
+
+   
+
+}
